8bd04d94c8cdc833eb8485b5694359e79b926968
Michael Lackey <michael.g.lackey@gmail.com>
2022-04-20 20:21:44 -0500

    add solution

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ const App = () => {
       <header className="kanye-header"></header>
       <main>
         <div className="quote-container">
-          <h1>Kanye Quotes</h1>
           {displayQuote ? (
             <KanyeQuote displayQuote={displayQuote} />
           ) : (
